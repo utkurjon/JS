f2132964756bec8224900ffbf6b3654cbf51dd17
utkur <utkur@bk.ru>
2022-02-27 19:18:01 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,22 @@
+var yourName = prompt ('Введите свое имя');
+    yourAge = +prompt ('Введите свой возраст');
+    example1 = +prompt ('Напишите ответ сложение: 5 + 7 = ');
+    example2 = +prompt ('Напишите ответ вычитание: 7 - 5 = ');
+    example3 = +prompt ('Напишите ответ умножение: 5 * 7 = ');
+    example4 = +prompt ('Напишите ответ деление: 35 / 7 = ');
+    example5 = +prompt ('Напишите ответ : 35 % 20 = ');
+
+    alert('Заходите в console и узнайте правильность ваших ответов');
+
+
+    console.log(`Ваше имя: ${yourName}\ Ваш возраст: ${yourAge}`);
+    console.log(`Задача 1: 5 + 7 = 12  (Ваш ответ: ${example1})`);
+    console.log(`Задача 2: 7 - 5 = 2   (Ваш ответ: ${example2})`);
+    console.log(`Задача 3: 5 * 7 = 35  (Ваш ответ: ${example3})`);
+    console.log(`Задача 4: 35 / 7 = 5  (Ваш ответ: ${example4})`);
+    console.log(`Задача 5: 35 % 20 = 15 (Ваш ответ: ${example5})`);
+
+
+
+
+
